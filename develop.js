73f2e919aa1adc8f20caae72bda355afe85a5f33
JujuSassaki <julianasassaki86@gmail.com>
2025-07-24 20:06:07 -0300

    bug:resolvendo bug do limpar formulário

--- develop.js
+++ develop.js
@@ -1,26 +1,27 @@
-const form = document.querySelector("form");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const idade = document.getElementById("idade");
-const curso = document.getElementById("curso");
-const enviar = document.getElementById("enviar");
+document.addEventListener("DOMContentLoaded", function () {
+  const form     = document.querySelector("form")
+  const nome     = document.getElementById("nome")
+  const email    = document.getElementById("email")
+  const idade    = document.getElementById("idade")
+  const curso    = document.getElementById("curso")
+  const output   = document.getElementById("output")
+  const btnReset = document.getElementById("btnReset")
 
-const output = document.querySelector("output");
-const reset = document.getElementById("reset");
+  function limparFormulario() {
+    form.reset()
+    output.innerHTML = ""
+  }
 
-reset.addEventListener("click", () => {
-    form.reset();
-    output.innerHTML = "";
-});
+  btnReset.addEventListener("click", limparFormulario)
 
-form.addEventListener("submit", (e) => {
-
-    e.preventDefault();
-
-    nomeValue = nome.value;
-    emailValue = email.value;
-    idadeValue = idade.value;
-    cursoValue = curso.value;
-
-    output.innerHTML = `Cadastro feito! Nome: ${nomeValue}, Email: ${emailValue}, Idade: ${idadeValue}, Curso: ${cursoValue} `
+  form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    output.innerHTML = `
+      Cadastro feito!<br>
+      Nome: ${nome.value}<br>
+      Email: ${email.value}<br>
+      Idade: ${idade.value}<br>
+      Curso: ${curso.value}
+    `
+  })
 })
